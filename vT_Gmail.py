b47d1cb3988fcfaeab061a4a3ccc389109271cda
sshah254 <sandipshah@vthrive.com>
2015-02-20 21:32:38 -0800

    Update vT_Gmail.py
    Added the code to use service accounts.

--- vT_Gmail.py
+++ vT_Gmail.py
@@ -9,7 +9,52 @@ import argparse
 import sys
 
 
+#admin@presgroup.net
+#scopes - https://developers.google.com/gmail/api/auth/scopes
+
+
+
+import pprint
+import sys
+
+from apiclient.discovery import build
+from oauth2client.client import SignedJwtAssertionCredentials
+
+def initSA(service_account_email, service_account_pkcs12_file_path, user_email):
+  """Build and returns a Gmail service object authorized with the service accounts that act on behalf of the given user.
+
+  Args:
+    user_email: The email of the user.
+  Returns:
+    Gmail service object.
+  """
+  
+  f = file(service_account_pkcs12_file_path, 'rb')
+  key = f.read()
+  f.close()
+
+  # Check https://developers.google.com/gmail/api/auth/scopes for all available scopes
+  # OAUTH_SCOPE='https://mail.google.com/'
+  OAUTH_SCOPE = 'https://www.googleapis.com/auth/gmail.readonly'
+  
+  credentials = SignedJwtAssertionCredentials(service_account_email, key, scope=OAUTH_SCOPE, sub=user_email)
+  http = httplib2.Http()
+  http = credentials.authorize(http)
+
+  gmail_service = build('gmail', 'v1', http=http)
+
+  return gmail_service
+
+
+
 def init(csf):
+  """Build and returns a Gmail service object authorized with the client id for a native application.
+
+  Args:
+    csf = client secret file in json format
+  Returns:
+    Gmail service object.
+  """
 
   # Path to the client_secret.json file downloaded from the Developer Console
   CLIENT_SECRET_FILE = csf
@@ -57,7 +102,6 @@ def vTgSearch(gmail_service, u, l, ist, m, q):
       print 'Message\nFrom: %s\nTo: %s\nSubject: %s\nDate: %s\n%s' % (from1, to1, subject1, date1, snippet1)
 
 
-
 if __name__ == "__main__":
 
   try:
@@ -73,6 +117,9 @@ if __name__ == "__main__":
     parser.add_argument('-q', '--queryFor', required=False, default='Gmail', help='search for, default Gmail')
     parser.add_argument('-l', '--label', required=False, default='INBOX', help='Label / folder to search in, default INBOX')
     parser.add_argument('-i', '--includeSpamTrash', required=False, default=False, help='Include Spam/Trash in search, default False')
+    parser.add_argument('-sa', '--useServiceAccount', required=False, default=False, help='Use Service Account')
+    parser.add_argument('-sae', '--SAEmail', required=False, help='Service Account Email address')
+    parser.add_argument('-k', '--key', required=False, default='key.p12', help='PKCS12 Key File Path, default key.p12')
     
     vT_args = parser.parse_args()
     
@@ -90,10 +137,15 @@ if __name__ == "__main__":
   if isinstance(vT_args.label, basestring):
     vT_args.label = vT_args.label.upper()
   
-  
-  gmail_service = init(csf=vT_args.clientSecretFile)
-  
-  print vT_args
+  if isinstance(vT_args.useServiceAccount, basestring):
+    vT_args.useServiceAccount = vT_args.useServiceAccount.upper()
+
+
+  if vT_args.useServiceAccount:
+    gmail_service = initSA(service_account_email = vT_args.SAEmail, service_account_pkcs12_file_path = vT_args.key, user_email = vT_args.userId)
+  else:
+    gmail_service = init(csf=vT_args.clientSecretFile)
+
   vTgSearch(gmail_service=gmail_service, u = vT_args.userId, l = vT_args.label, ist = vT_args.includeSpamTrash, m = vT_args.maxResults, q = vT_args.queryFor)
   
   print('---Done---')
